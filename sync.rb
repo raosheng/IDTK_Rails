e0b38c242cfb1476182a4a61079697aa830822fa
Li Xinyang <lixinyang1026@gmail.com>
2016-10-10 16:14:40 +0800

    Update

--- sync.rb
+++ sync.rb
@@ -1,4 +1,14 @@
+# Fetch remote resources
 `git pull origin dev`
 `git add --all`
+
+# Create README File Flashback Version
+File.open('README.md', 'a') do |f|
+end
+
+# Create README File Normal Version
+
+# Update remote resources
 `git commit -m "Update"`
 `git push origin dev`
+
