9351d29a81bdc1058e9cac07bb5e0d0ad10addf0
Li Xinyang <lixinyang1026@gmail.com>
2016-10-10 16:44:26 +0800

    Update

--- sync.rb
+++ sync.rb
@@ -2,19 +2,19 @@ def append_content_from(target_dir, file_path, order="DES")
   queue = []
 
   Dir.foreach(target_dir) do |file_name|
-    next if file_name == '.' or file_name == '..'
+    next if file_name == "." or file_name == ".."
     queue << "#{target_dir}/#{file_name}"
   end
 
-  if order == 'DES'
+  if order == "DES"
     queue.sort!
-  elsif order == 'ASC'
+  elsif order == "ASC"
     queue.sort!.reverse!
   end
 
   queue.each do |path|
-    File.open(file_path, 'a') do |f|
-      File.open(path, 'r') do |source_file|
+    File.open(file_path, "a") do |f|
+      File.open(path, "r") do |source_file|
         f.write(source_file.read)
         f.puts @string
       end
@@ -28,13 +28,13 @@ end
 
 # Create README File Flashback Version
 `rm README.md`
-append_content_from('./content', './README.md')
-append_content_from('./log', './README.md', 'ASC')
+append_content_from("./content", "./README.md")
+append_content_from("./log", "./README.md", "ASC")
 
 # Create README File Normal Version
 `rm README_Humanize.md`
-append_content_from('./content', './README_Humanize.md')
-append_content_from('./log', './README_Humanize.md')
+append_content_from("./content", "./README_Humanize.md")
+append_content_from("./log", "./README_Humanize.md")
 
 # Update remote resources
 `git commit -m "Update"`
