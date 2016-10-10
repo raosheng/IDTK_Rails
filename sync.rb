4ec43fb903639ac141862608ea35ec4abc67364a
Li Xinyang <lixinyang1026@gmail.com>
2016-10-10 16:16:33 +0800

    Update

--- sync.rb
+++ sync.rb
@@ -3,10 +3,14 @@
 `git add --all`
 
 # Create README File Flashback Version
+`rm README.md`
 File.open('README.md', 'a') do |f|
 end
 
 # Create README File Normal Version
+`rm README_Humanize.md`
+File.open('README_Humanize.md', 'a') do |f|
+end
 
 # Update remote resources
 `git commit -m "Update"`
